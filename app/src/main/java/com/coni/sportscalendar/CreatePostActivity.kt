df8f1bfbc2190da70aa9d5af1186300607de3ca5
Domiz69 <ProgramingDomiz69@users.noreply.github.com>
2020-04-14 02:08:46 +0300

    Added ability to create  posts

---
+++ app/src/main/java/com/coni/sportscalendar/CreatePostActivity.kt
@@ -0,0 +1,11 @@
+package com.coni.sportscalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+    }
+}
